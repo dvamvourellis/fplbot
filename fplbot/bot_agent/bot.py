133477fe67775d7eb9e0f4d081f10828769839db
Dimitris Vamvourellis <dimitrisvamvourellis@Dimitriss-MBP-2.lan>
2023-09-04 15:36:29 -0400

    added pandas agent with memory

--- fplbot/bot_agent/bot.py
+++ fplbot/bot_agent/bot.py
@@ -6,15 +6,21 @@ import pickle
 sys.path.append("../")
 
 from fplbot.bot_agent.llm_constructors import init_davinci, init_gpt35_turbo
-from fplbot.knowledge_base.data_utils import process_current_data, get_fixture_data, create_player_history_agg
+from fplbot.knowledge_base.data_utils import (
+    process_current_data,
+    get_fixture_data,
+    create_player_history_agg,
+)
 from fplbot.utils import get_env_variable
 from fplbot.bot_agent.pandas_agent_with_memory import create_pandas_multidf_with_memory
+from fplbot.bot_agent.templates import MULTI_DF_PREFIX, MULTI_DF_PREFIX_WITH_HIST
 
 from langchain.agents import Tool
 from langchain.agents import initialize_agent
 from langchain.agents import create_pandas_dataframe_agent
 
-DATA_DIR = get_env_variable('data_dir')
+DATA_DIR = get_env_variable("data_dir")
+
 
 class fplAgent:
     def __init__(
@@ -22,12 +28,14 @@ class fplAgent:
         openai_api_key: Optional[str] = None,
         model_name: Optional[str] = "gpt-3.5-turbo",
         update_data=False,
+        load_history=False,
         return_intermediate_steps: bool = True,
         verbose: bool = True,
     ):
         self.verbose = verbose
         self.return_intermediate_steps = return_intermediate_steps
         self.update_data = update_data
+        self.load_history = load_history
 
         self._init_llm(openai_api_key, model_name)
 
@@ -54,29 +62,41 @@ class fplAgent:
             )
 
     def _fetch_data(self):
-        if self.update_data==True:
+        if self.update_data == True:
             data_dict = process_current_data(update_data=self.update_data)
-            player_data_agg_dict_final = create_player_history_agg(data_dict['id2player'], data_dict['id2team'], update_data=self.update_data)
         else:
-            with open(DATA_DIR+'players_dict.pickle', "rb") as f:
+            with open(DATA_DIR + "players_dict.pickle", "rb") as f:
                 data_dict = pickle.load(f)
-            with open(DATA_DIR+'players_history_dict.pickle', "rb") as f:
-                player_data_agg_dict_final = pickle.load(f)
 
         self.teams_df = data_dict["teams"]
         self.players_df = data_dict["players"]
         self.gameweek_stats_df = data_dict["gameweek_stats"]
         self.fixtures_df = data_dict["fixtures"]
 
-        self.players_season_history = player_data_agg_dict_final['players_season_history']
-        self.players_gw_history = player_data_agg_dict_final['players_gw_history']
+        if self.load_history == True:
+            if self.update_data == True:
+                player_data_agg_dict_final = create_player_history_agg(
+                    data_dict["id2player"],
+                    data_dict["id2team"],
+                    update_data=self.update_data,
+                )
+            else:
+                with open(DATA_DIR + "players_history_dict.pickle", "rb") as f:
+                    player_data_agg_dict_final = pickle.load(f)
+
+            self.players_season_history = player_data_agg_dict_final[
+                "players_season_history"
+            ]
+            self.players_gw_history = player_data_agg_dict_final["players_gw_history"]
 
     def _init_agent(self):
-        dfs = [self.players_df, self.fixtures_df, self.teams_df, self.gameweek_stats_df, 
-               self.players_season_history, self.players_gw_history]
-        
+        dfs = [self.players_df, self.fixtures_df, self.teams_df, self.gameweek_stats_df]
+        prefix = MULTI_DF_PREFIX
+        if self.load_history == True:
+            dfs.append(self.players_season_history)
+            dfs.append(self.players_gw_history)
+            prefix = MULTI_DF_PREFIX_WITH_HIST
+
         self.agent = create_pandas_multidf_with_memory(
-            self.llm, 
-            dfs, 
-            verbose=self.verbose
-            )
+            self.llm, dfs, verbose=self.verbose, prefix=prefix
+        )
--- fplbot/bot_agent/templates.py
+++ fplbot/bot_agent/templates.py
@@ -1,4 +1,4 @@
-MULTI_DF_PREFIX = """
+MULTI_DF_PREFIX_WITH_HIST = """
 You are working with {num_dfs} pandas dataframes in Python named df1, df2, df3, df4, df5, df6.
 Dataframe `df1` is useful for when you need to answer questions about each player using \
 the most current view of his aggregate statistics up to this gameweek and his current cost. 
@@ -14,6 +14,20 @@ any player for previous seasons.
 Dataframe `df6` is useful for when you need to answer questions about the statistics of \
 any player for previous gameweeks from this season. The columns of this dataframe reflect the statistics \
 achieved by each player in each individual gameweek.
+You should use the tools below to answer the question posed of you:
+    """
+
+MULTI_DF_PREFIX = """
+You are working with {num_dfs} pandas dataframes in Python named df1, df2, df3, df4.
+Dataframe `df1` is useful for when you need to answer questions about each player using \
+the most current view of his aggregate statistics up to this gameweek and his current cost. 
+Dataframe `df2` is useful for when you need to answer questions about the past and \
+future fixtures of each team and the difficulty of them. 
+Dataframe `df3` is useful for when you need to answer questions \
+related to how strong each team is at home or away or in terms of attack and defence.
+Dataframe `df4` is useful for when you need to answer questions about the statistics of \
+each FPL gameweek such as the average score achieved by users and the player that was most captained \
+or most selected.
 You should use the tools below to answer the question posed of you:
     """
 
